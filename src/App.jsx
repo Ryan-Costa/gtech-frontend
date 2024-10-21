4353c189a9fc0b761febb90489d3a51989f58046
Ryan <ryansouzadev@gmail.com>
2024-10-21 10:51:30 -0300

    feat slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./index.css"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
+import Slide from "./components/Slider"
 
 function App() {
 
   return (
     <div className="h-screen">
       <Header />
-      
+      <Slide />
       <Footer />
     </div>
   )
