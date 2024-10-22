d35bba1c0777d32c5793d3656b16373fc581af24
Ryan <ryansouzadev@gmail.com>
2024-10-22 11:28:38 -0300

    feat: refactory highlights components

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Destaques from "./components/Destaques"
 function App() {
 
   return (
-    <div className="h-screen">
+    <div className="h-screen bg-[#F9F8FE]">
       <Header />
       <Slide />
       <Destaques />
